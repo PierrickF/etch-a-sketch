fa330f9b14cf99e0ed13fcc59e9a10b16fe2c075
Pierrick F <pierrickfoulquier@gmail.com>
2021-03-01 11:41:18 +0100

    Made the grid flexible and composed of squares of egal size (no overlap).

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,28 @@
 // container for the grid
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");              // create 16 rows
-    //square.style.backgroundColor = "pink";
-    //square.style.borderStyle = "solid";
-    //square.style.borderColor = "black";
-    //square.style.borderWidth = "1px";
-    row.textContent = 0;                                    // CSS placeholder so that something is displayed
-    row.style.display = "flex";                             // make sure it is a grid
-    container.appendChild(row);                             // place the div in the DOM
+for (let i = 0; i < 16; i++) {                                  // create 15 columns
 
-    for (let i = 1; i < 16; i++) {                               
-        const column = document.createElement("div");       // create 16 columns
-        // square2.style.backgroundColor = "pink";
-        // square2.style.borderStyle = "solid";
-        // square2.style.borderColor = "black";
-        // square2.style.borderWidth = "1px";
-        column.textContent = i;                             // CSS placeholder so that something is displayed
-        row.appendChild(column);                            // place the div in the DOM
+    for (let j = 0; j < 15; j++) {                              // for each column     
+        const row = document.createElement("div");              // create 16 rows
+        row.style.backgroundColor = "pink";
+        row.style.borderStyle = "solid";
+        row.style.borderColor = "black";
+        row.style.borderWidth = "1px";
+        row.textContent = j;                                    // CSS placeholder so that something is displayed
+        row.style.height = "50px";
+        row.style.width = "50px";
+        container.appendChild(row);                             // place the div in the DOM
     }
+
+    const lastRow = document.createElement("div");              // create one last column
+    lastRow.style.backgroundColor = "pink";
+    lastRow.style.borderStyle = "solid";
+    lastRow.style.borderColor = "black";
+    lastRow.style.borderWidth = "1px";
+    lastRow.textContent = 15;                                   // CSS placeholder so that something is displayed
+    lastRow.style.height = "50px";
+    lastRow.style.width = "50px";
+    lastRow.style.breakAfter = "always";                        // force line break after this column
+    container.appendChild(lastRow);                             // place the div in the DOM
 }
