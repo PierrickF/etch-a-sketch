28c1bfd7455f9ae63dee72ebedff2fa5a767c618
Pierrick F <pierrickfoulquier@gmail.com>
2021-03-01 09:42:40 +0100

    Create a column of 16 rows (16 div) in the grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,10 @@
 // container for the grid
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {                          // do the following block 16 times
+    const square = document.createElement("div");       // create a div (a box in the grid)
+square.style.backgroundColor = "pink";                  // apply some CSS
+square.textContent = "placeholder";                     // apply some CSS
+
+container.appendChild(square);                          // place the div in the DOM
+}
