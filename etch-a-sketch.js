1a09b0f69346964c49602d7580d49346bc49af89
Pierrick F <pierrickfoulquier@gmail.com>
2021-03-02 15:05:14 +0100

    Moved some CSS rules from .js to .css by creating classes.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,22 +10,14 @@ for (let i = 0; i < size; i++) {                                // create 15 col
     for (let j = 0; j < size - 1; j++) {                        // for each column     
         const row = document.createElement("div");              // create 16 rows
         row.id = `square${dracula}`;                            // give a unique id to each square on grid
-        row.style.borderStyle = "solid";
-        row.style.borderColor = "black";
-        row.style.borderWidth = "1px";
-        row.style.flexGrow = 1;
+        row.classList.add("row");                               // apply some CSS
         container.appendChild(row);                             // place the div in the DOM
         dracula++;
     }
 
     const lastRow = document.createElement("div");              // create one last column
     lastRow.id = `square${dracula}`;                            // give a unique id to each square on grid
-    lastRow.style.borderStyle = "solid";
-    lastRow.style.borderColor = "black";
-    lastRow.style.borderWidth = "1px";
-    lastRow.style.flexGrow = 1;
-    // THE NEXT LINE IS IGNORED BY GOOGLE CHROME
-    lastRow.style.breakAfter = "always";                        // force line break after this column
+    lastRow.classList.add("lastRow");                           // apply some CSS
     container.appendChild(lastRow);                             // place the div in the DOM
     dracula++;
 }
